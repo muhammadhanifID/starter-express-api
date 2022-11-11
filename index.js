3e9ded0703ea756cfb882be02e78192bff8a8a68
muhammadhanifID <mhd.hanif.id@gmail.com>
2022-11-11 21:41:43 +0700

    error

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+import express from "express";
+import fetch from "node-fetch";
+import FormData from "form-data";
+const app = express();
+app.all("/", async (req, res) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+	let formData = new FormData();
+	formData.append(
+		"key",
+		"ZFtrHZgY4lBAxPacQPiapdAFCCs6GQu3Vk8vnnzhrVSGqHm70msQG30UMxbgsx8B"
+	);
+	formData.append("sign", "2c1a72e53c246770b3f2aae2c80e3ff9");
+
+	// note: use npm install node-fetch@2.0 to be able to use "require"
+
+	const response = await fetch("https://reetenstore.id/api/profile", {
+		method: "post",
+		body: formData,
+	});
+	const data = await response.json();
+
+	console.log(data);
+	res.send(data);
+});
+app.listen(process.env.PORT || 4000);
